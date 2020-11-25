e8e350ecfe16b55bf4c0ecb894d260a1cd8e2f40
ckcheong2 <75022644+ckcheong2@users.noreply.github.com>
2020-11-25 21:29:29 +0800

    My 1st website - 25 Nov 2020 First

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <div class='mt-20'>
-      <Logo style='width:120px;margin:auto' />
-      <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
-      </h1>
-      <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
-      </h2>
-      
+  <div class="bg-grayish-white-1 relative">
+    <!-- Displaying Image -->
+    <div class="h-screen w-2/3">
+      <img
+        class="w-full h-full object-cover"
+        src="https://images.pexels.com/photos/331684/pexels-photo-331684.jpeg?cs=srgb&dl=pexels-math-331684.jpg&fm=jpg"
+      />
+    </div>
+    <! -- Displaying Name -->
+    <div class="absolute h-screen w-1/2 top-0 right-0">
+      <div class="flex h-full w-full bg-bluee-500">
+        <div class="my-auto w-full">
+          <div class="flex flex-col">
+            <div class="text-4x1 font-poppins text-gray-700">
+              <span class="font-semibold">Welcome to</span>
+              <span class="font-thin"> Eddie's </span>
+              <span class="font-semibold">Page.</span>
+            </div>
+            <div class="font-thin font-sans text-gray-700 text-center -mt-2">
+              You will be AMAZE !!!
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
